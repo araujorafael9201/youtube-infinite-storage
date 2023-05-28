2341ecb3c8530f86ab30042087c3d312706680e5
Rafael <araujorafael9201@gmail.com>
2023-05-28 15:32:36 -0300

    Can export .mp4 video using ffmpeg

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Youtube Infinite Storage
 ## This app transforms files of any type into videos so you can store everything you want on YouTube
----
-
+- Inspired by [this repo](https://github.com/DvorakDwarf/Infinite-Storage-Glitch)
 ## Usage
 ```
-./main input_file output_dir
+./main input_file output_file
 ```
+> Requires [FFmpeg](https://github.com/FFmpeg/FFmpeg)
--- main.c
+++ main.c
@@ -2,102 +2,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <unistd.h>
 
-#define max_columns 1280
-#define max_rows 720
-
-FILE *get_output_file(char *basename, int c) {
-  if (c == 0) {
-    mkdir(basename, 0700);
-  }
-
-  char *n = malloc(sizeof(char) * strlen(basename) + 5);
-  sprintf(n, "./%s/%d.ppm", basename, c);
-
-  FILE *f = fopen(n, "wb+");
-
-  return f;
-}
-
-void close_file(FILE *f, char *header, int row_count, int column_count) {
-  printf("%d\n", row_count);
-
-  // Fill the rest of the row with black
-  while (column_count <= max_columns) {
-    fwrite("0 0 0  ", strlen("0 0 0  "), 1, f);
-    column_count++;
-  }
-
-  // Write ppm header
-  fseek(f, 0, SEEK_SET);
-  sprintf(header, "P3\n%u %u\n255\n", column_count, row_count);
-  fwrite(header, strlen(header), 1, f);
-  fclose(f);
-}
+#define VIDEO_WIDTH 1280
+#define VIDEO_HEIGHT 720
+#define FRAMERATE 15
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
-    printf("Usage: %s <input file> <output file>\n", argv[0]);
+    printf("Usage: %s <input file> <output file (without extension)>\n",
+           argv[0]);
     return 1;
   }
 
-  FILE *fp = fopen(argv[1], "rb");
-  if (fp == NULL) {
-    printf("Error: cannot open file %s\n", argv[1]);
-    return 1;
+  // Create fork proccess to run ffmpeg + pipe to send information
+
+  int pipefd[2];
+  if (pipe(pipefd) == -1) {
+    printf("Error: Could not create pipe");
+    return EXIT_FAILURE;
   }
 
-  uint32_t column_count = 0;
-  uint32_t row_count = 1;
-  size_t output_file_count = 0;
+  pid_t pid = fork();
 
-  FILE *output = get_output_file(argv[2], output_file_count);
-  uint16_t *buffer = malloc(sizeof(uint8_t));
-
-  // Write PPM Header
-  char *header = malloc(sizeof("P3\n1280 720\n255\n"));
-  fseek(output, strlen(header), SEEK_CUR);
-
-  uint8_t value_counter = 0;
-  char *w = malloc(sizeof("255  "));
-
-  while (feof(fp) == 0) {
-    fread(buffer, sizeof(uint8_t), 1, fp);
-    sprintf(w, "%u ", *buffer);
-
-    fwrite(w, strlen(w), 1, output);
-
-    if (value_counter++ == 3) {
-      value_counter = 0;
-      column_count++;
-    }
-
-    if (column_count == max_columns) {
-      // Next Row
-      column_count = 0;
-      fwrite("\n", sizeof(char), 1, output);
-      row_count++;
-    }
-
-    if (row_count >= 720) {
-      close_file(output, header, row_count, column_count);
-
-      output_file_count++;
-      output = get_output_file(argv[2], output_file_count);
-      fseek(output, strlen(header), SEEK_CUR);
-      row_count = 0;
-    }
+  if (pid == -1) {
+    printf("Error: Could not create child proccess!");
+    return EXIT_FAILURE;
   }
 
-  free(w);
-  free(buffer);
+  ssize_t bytes_read;
 
-  close_file(output, header, row_count, column_count);
+  if (pid == 0) {
+    // Parent proccess
+    // Read file + Send bytes to child
+    close(pipefd[0]);
 
-  free(fp);
-  free(header);
+    FILE *fd = fopen(argv[1], "rb");
+    if (fd == NULL) {
+      printf("Error: cannot open file %s\n", argv[1]);
+      return 1;
+    }
+
+    uint32_t frame[VIDEO_WIDTH * VIDEO_HEIGHT];
+    while (!feof(fd)) {
+      bytes_read = fread(frame, sizeof(frame), 1, fd);
+      write(pipefd[1], frame, sizeof(frame));
+    }
+
+    close(pipefd[0]);
+    fclose(fd);
+  } else {
+    // Child proccess
+    // Receive bytes + run ffmpeg to create video
+    close(pipefd[1]);
+    dup2(pipefd[0], STDIN_FILENO);
+    char *output_file = malloc(32);
+    sprintf(output_file, "%s.mp4", argv[2]);
+
+    char *resolution = malloc(15);
+    sprintf(resolution, "%dx%d", VIDEO_WIDTH, VIDEO_HEIGHT);
+
+    char *framerate_str = malloc(5);
+    sprintf(framerate_str, "%d", FRAMERATE);
+
+    int r;
+    if ((r = execlp("ffmpeg", "-y", "-f", "rawvideo", "-pix_fmt", "rgb32", "-s",
+                    resolution, "-r", framerate_str, "-i", "-", "-c:v",
+                    "libx264", output_file, NULL) == -1)) {
+      printf("Error: Could not execute ffmpeg\n");
+      return EXIT_FAILURE;
+    }
+
+    close(pipefd[0]);
+  }
 
   return 0;
 }
